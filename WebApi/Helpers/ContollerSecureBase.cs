2e93e0a7ea9f79c1c6bd71f3996103027c81256c
acid0ikario <1mm.villanueva1@gmail.com>
2020-01-19 20:58:11 -0600

    clase para manejar informacion del JWT token

---
+++ WebApi/Helpers/ContollerSecureBase.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Helpers
+{
+    public abstract class ContollerSecureBase: ControllerBase
+    {
+       
+
+        public bool isAdminUser(string token, string strkey)
+        {
+           
+            var key = Encoding.ASCII.GetBytes(strkey);
+            var handler = new JwtSecurityTokenHandler();
+            var validations = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+           
+            var claims = handler.ValidateToken(token, validations, out var tokenSecure);
+
+            if( claims.Claims.FirstOrDefault(x => x.Type == "rolId").Value == "ADM")
+                return true;
+
+            return false;
+        }
+
+        public string GetUsuarioToken(string token, string strkey)
+        {
+
+            var key = Encoding.ASCII.GetBytes(strkey);
+            var handler = new JwtSecurityTokenHandler();
+            var validations = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+
+            var claims = handler.ValidateToken(token, validations, out var tokenSecure);
+
+            return claims.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.UniqueName).Value;
+                
+        }
+
+        public string GetTokenAuth(HttpRequest re) {
+            var header = Request.Headers.First(x => x.Key == "Authorization");
+            string token = header.Value;
+            token = token.Replace("Bearer ", "");
+            return token;
+        }
+
+    }
+}
