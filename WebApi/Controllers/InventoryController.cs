9c1b2f70f6b6672bfc1c741c82b1e17fa289e90e
acid0ikario <1mm.villanueva1@gmail.com>
2020-01-19 20:57:40 -0600

    metodos para manejar el inventario

--- WebApi/Controllers/InventoryController.cs
+++ WebApi/Controllers/InventoryController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using Repository.Interfaces;
 using WebApi.Helpers;
 using Microsoft.Extensions.Configuration;
+using DataAccess.Entities;
 
 namespace WebApi.Controllers
 {
@@ -30,15 +31,54 @@ namespace WebApi.Controllers
         }
 
         // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        [HttpGet("GetListItems")]
+        public ActionResult<IEnumerable<string>> GetListItems(string sku = "")
         {
-             string secretKey = _configuration.GetValue<string>("DevSecretKey");
+            string secretKey = _configuration.GetValue<string>("DevSecretKey");
             var re = Request;
             var header = Request.Headers.First(x => x.Key == "Authorization");
             return Ok(isAdminUser(header.Value, secretKey));
         }
 
-        
+        // GET api/values
+        [HttpPost("addItem")]
+        public ActionResult<IEnumerable<string>> addItem([FromBody] Items item)
+        {
+           
+            string secretKey = _configuration.GetValue<string>("DevSecretKey");
+            if (isAdminUser(GetTokenAuth(Request ), secretKey))
+            {
+                Items returnedItem = _Inventory.CreateItem(item);
+                return Ok(returnedItem);
+            }
+
+            return Unauthorized("No esta autorizado para realizar esta accion");
+        }
+
+        [HttpPost("UpdateItem")]
+        public ActionResult<IEnumerable<string>> UpdateItem([FromBody] Items item)
+        {
+
+            string secretKey = _configuration.GetValue<string>("DevSecretKey");
+            if (isAdminUser(GetTokenAuth(Request), secretKey))
+            {
+                Items returnedItem = _Inventory.UpdateItem(item);
+                return Ok(returnedItem);
+            }
+
+            return Unauthorized("No esta autorizado para realizar esta accion");
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetListaInventario()
+        {
+            string secretKey = _configuration.GetValue<string>("DevSecretKey");
+            var re = Request;
+            var header = Request.Headers.First(x => x.Key == "Authorization");
+            return Ok(isAdminUser(header.Value, secretKey));
+        }
+
     }
 }
