62aaf130d215e0f3b15ff96b84772622551f9183
acid0ikario <1mm.villanueva1@gmail.com>
2020-01-19 20:07:00 -0600

    Creo metodos para leer los claims y saber si el usuario el admin.

--- WebApi/Controllers/InventoryController.cs
+++ WebApi/Controllers/InventoryController.cs
@@ -7,27 +7,38 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Repository.Interfaces;
+using WebApi.Helpers;
+using Microsoft.Extensions.Configuration;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    public class InventoryController : ControllerBase
+    public class InventoryController : ContollerSecureBase
     {
 
         private readonly IInventoryRepository _Inventory;
+        private readonly IConfiguration _configuration;
 
-        public InventoryController(IInventoryRepository inventory)
+        public InventoryController(IInventoryRepository inventory, IConfiguration config)
         {
             _Inventory = inventory;
+            _configuration = config;
+
+
         }
 
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+             string secretKey = _configuration.GetValue<string>("DevSecretKey");
+            var re = Request;
+            var header = Request.Headers.First(x => x.Key == "Authorization");
+            return Ok(isAdminUser(header.Value, secretKey));
         }
+
+        
     }
 }
--- WebApi/Controllers/OrdersController.cs
+++ WebApi/Controllers/OrdersController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +11,7 @@ namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class OrdersController : ControllerBase
     {
 
